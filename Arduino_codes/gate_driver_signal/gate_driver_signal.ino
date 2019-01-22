f6ab7c3aae65dba256dc3e849f9b2b95e5a93d3c
Ali Aydin <ali.aydin@metu.edu.tr>
2019-01-22 21:54:13 +0300

    Arduino is fixed for IGBT.

--- Arduino_codes/gate_driver_signal/gate_driver_signal.ino
+++ Arduino_codes/gate_driver_signal/gate_driver_signal.ino
@@ -1,29 +1,13 @@
 
-const byte pulse_a = 7;
-const byte interruptPin = 2;
-const byte pulse_a_180 = 8;
-const byte interruptPin2 = 3;
+const byte analogInput = A0;
+const byte pwmOutput = 3;
 
 void setup() {
-  pinMode(pulse_a, OUTPUT);
-  pinMode(interruptPin, INPUT);
-  pinMode(pulse_a_180, OUTPUT);
-  pinMode(interruptPin2, INPUT);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), pulse_1_2, RISING);
-  attachInterrupt(digitalPinToInterrupt(interruptPin2), pulse_3_4, RISING);
+  pinMode(pwmOutput, OUTPUT);
+  pinMode(analogInput, INPUT);
 }
 
 void loop() {
-  
-}
-void pulse_1_2(){
-  digitalWrite(pulse_a, HIGH);
-  delay(1); // milisecond %5 pulse width
-  digitalWrite(pulse_a, LOW);
-}
-void pulse_3_4(){
-  digitalWrite(pulse_a_180, HIGH);
-  delay(1); // milisecond %5 pulse width
-  digitalWrite(pulse_a_180, LOW);
-
+  float value = analogRead(analogInput) / 5;
+  analogWrite(pwmOutput, value);
 }
